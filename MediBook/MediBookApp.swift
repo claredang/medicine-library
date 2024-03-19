d9f2e0b7de0e043e477250d0b1ecbf1468c90585
Clare Dang <claredng@gmail.com>
2024-03-19 17:37:21 +0700

    Initial Commit

---
+++ MediBook/MediBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediBookApp.swift
+//  MediBook
+//
+//  Created by Clare Dang on 2024-03-19.
+//
+
+import SwiftUI
+
+@main
+struct MediBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
